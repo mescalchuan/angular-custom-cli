4cc4e211312313cf8af487eac463d8f0632e60d3
Mescal Chuan <1335382915@qq.com>
2018-03-09 17:39:07 +0800

    Update app.js

--- common/app.js
+++ common/app.js
@@ -1,6 +1,6 @@
 import '../css/basic.scss';
 
 export const instruction = () => ({
-	restrict: 'EACM',
-	template: `<p class="instruction">This project created by angular-m-cli.</p>`
+    restrict: 'EACM',
+    template: `<p class="instruction">This project created by angular-m-cli.</p>`
 });
