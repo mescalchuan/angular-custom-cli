b1075ad6d2cba0626dd29d7ad8631537d15a0769
Mescal Chuan <1335382915@qq.com>
2018-03-09 17:37:54 +0800

    Update main.js

--- entry/home/main.js
+++ entry/home/main.js
@@ -3,16 +3,16 @@ import { instruction } from '../../common/app';
 import '../../css/home.scss';
 
 const homeCtrl = $scope => {
-	$scope.pageInfo = 'Hello Angular';
+    $scope.pageInfo = 'Hello Angular';
 }
 const helloNG = () => ({
-	restrict:'EACM',
-	template:`<p class="home-title">{{pageInfo}}</p>`
+    restrict:'EACM',
+    template:`<p class="home-title">{{pageInfo}}</p>`
 });
 
 angular.module('home', [])
-	.controller('homeCtrl',['$scope', 
-		$scope => homeCtrl($scope)
-	])
-	.directive('helloNg', helloNG)
-	.directive('ngText', instruction);
+    .controller('homeCtrl',['$scope', 
+        $scope => homeCtrl($scope)
+    ])
+    .directive('helloNg', helloNG)
+    .directive('ngText', instruction);
